03cc6fcf3c7b75303778fb043c073e999f70333c
Jim Rudolf <rudolf@jbrcodes.com>
2026-05-28 06:19:54 +0200

    Check for "beta languages" no longer necessary

--- bn/lib/DeepLTranslator.py
+++ bn/lib/DeepLTranslator.py
@@ -3,9 +3,6 @@
 import deepl
 
 
-LangsBeta = ('fa', 'hi')  # Persian, Hindi
-
-
 deepl_client = None
 
 
@@ -19,10 +16,7 @@ class DeepLTranslator():
 
     def translate_strings(self, strs_src, lang_src, lang_dest):
         lang_dest1 = 'en-us' if lang_dest == 'en' else lang_dest
-        if lang_src in LangsBeta or lang_dest1 in LangsBeta:
-            results = deepl_client.translate_text(strs_src, source_lang=lang_src, target_lang=lang_dest1, \
-                extra_body_parameters={ 'enable_beta_languages': True })
-        else:
-            results = deepl_client.translate_text(strs_src, source_lang=lang_src, target_lang=lang_dest1)
+        results = deepl_client.translate_text(strs_src, source_lang=lang_src, target_lang=lang_dest1)
         strs_dest = [r.text for r in results]
+        
         return strs_dest
